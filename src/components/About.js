e96c51c1b63e6dc1e127ef506ec55706106fbed8
AdeelAtta <adeelatta@gmail.com>
2023-10-21 14:58:31 +0500

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,12 @@ const About = () => {
                 <div className='expand_50 left margin_16'>
                     <p className='font_16 light t_purple mb_16'><FormatText text={'pages.index.section.aboutus.title'} /></p>
                     <h3 className='font_33 bold t_back mb_16'><FormatText text={'pages.index.section.aboutus.company-intro'} /> </h3>
-                    <p className='font_13 light t_gray mt_16 mr_16 mb_16'><FormatText text={'pages.index.section.aboutus.company-detail'} /></p>
+                    <p className='font_13 light t_gray mt_16 mr_16 mb_16'><FormatText text={'pages.index.section.aboutus.company-detail'} /><br/><FormatText text={'pages.index.section.aboutus.company-detail2'} /></p>
+                    
                     <ul className='mt_16 mb_50 ml_16 '>
                         <li className='flex items-center gap-3 font_13 light t_black margin_8'><FiCheck className='t_golden ' /> Canada</li>
-                        <li className='flex items-center font_13 light t_black margin_8'><FiCheck className='t_golden ' /> Unites States</li>
-                        <li className='flex items-center font_13 light t_black margin_8'>& Many more...</li>
+                        <li className='flex items-center gap-3 font_13 light t_black margin_8'><FiCheck className='t_golden ' /> Unites States</li>
+                        <li className='flex items-center gap-3 font_13 light t_black margin_8'>& Many more...</li>
                     </ul>
                     <Link className='btn bg_golden t_white ' href='/about'>More Details</Link>
                 </div>
